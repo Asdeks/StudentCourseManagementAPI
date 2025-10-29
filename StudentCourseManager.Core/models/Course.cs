d3a6be567ecce116eeb2b4c8cadeea8a7f229b79
Maksym Burlaka <maxspysadow@gmail.com>
2025-10-29 23:42:33 +0100

    clean debugg

---
+++ StudentCourseManager.Core/models/Course.cs
@@ -0,0 +1,9 @@
+namespace StudentCourseManager.Core.Models
+{
+    public class Courses
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
